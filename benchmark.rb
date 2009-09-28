b5128daba816af99f130a6827b8d1a422f1fef0c
Tom Tsuruhara <tom.lpsd@gmail.com>
2009-09-29 00:13:08 +0900

    added benchmark for erb and erubis

--- benchmark.rb
+++ benchmark.rb
@@ -1,8 +1,11 @@
 require 'benchmark'
+require 'erb'
+require 'erubis'
 require 'html/template/pro'
 
 varset1 = { :VAR1 => 'VAR1', :VAR2 => 'VAR2', :VAR3 => 'VAR3', :VAR10 => 'VAR10' }
-varset2 = {:STUFF1 => '<>"; %FA' }
+varset2 = { :STUFF1 => '<>"; %FA' }
+
 refset1 = {
   :HASHREF0=>[],
   :HASHREF2=>[{},{}],
@@ -13,6 +16,8 @@ refset1 = {
               {:LOOPVAR1=>'LOOP4-VAR1',:LOOPVAR2=>'LOOP4-VAR2',:LOOPVAR3=>'LOOP4-VAR3',:LOOPVAR10=>'LOOP4-VAR10'},
              ]
 }
+
+VAR1, VAR2, VAR3, VAR10 = %w{VAR1 VAR2 VAR3 VAR10}
 
 # -------------------------
 
@@ -28,6 +33,7 @@ end
 def test_tmpl_output(testname, params)
   Dir.chdir 'templates-Pro'
   file = testname  
+  count = 1000
 
   tmplo = HTML::Template::Pro.new(:filename => file + '.tmpl',
                                   :die_on_bad_params => false,
@@ -37,9 +43,8 @@ def test_tmpl_output(testname, params)
                                   )
   tmplo.param(params);
 
-  count = 1000
-
   File.open('/dev/null', 'w') do |file|
+    puts "Template::Pro " + '-' * 40
     puts Benchmark::CAPTION
     puts Benchmark.measure{
       count.times do
@@ -47,6 +52,29 @@ def test_tmpl_output(testname, params)
       end
     }
   end
+
+  erb = ERB.new(File.read(file + '.erb'))
+  File.open('/dev/null', 'w') do |file|
+    puts "ERB " + '-' * 40
+    puts Benchmark::CAPTION
+    puts Benchmark.measure{
+      count.times do
+        file.puts erb.result(binding)
+      end
+    }
+  end
+
+  erubis = Erubis::FastEruby.new(File.read(file + '.erb'))
+  File.open('/dev/null', 'w') do |file|
+    puts "Erubis " + '-' * 40
+    puts Benchmark::CAPTION
+    puts Benchmark.measure{
+      count.times do
+        file.puts erubis.result(binding)
+      end
+    }
+  end
+
   Dir.chdir '..'
 end
 
@@ -54,7 +82,9 @@ def test_tmpl_complete(testname, params)
   Dir.chdir 'templates-Pro';
   file = testname
   count = 1000
+
   File.open('/dev/null', 'w') do |file|
+    puts "Template::Pro " + '-' * 40
     puts Benchmark::CAPTION
     puts Benchmark.measure{
       count.times do
@@ -67,11 +97,36 @@ def test_tmpl_complete(testname, params)
       end
     }
   end
+
+  File.open('/dev/null', 'w') do |file|
+    puts "ERB " + '-' * 40
+    puts Benchmark::CAPTION
+    puts Benchmark.measure{
+      count.times do
+        erb = ERB.new(File.read(testname + '.erb'))
+        file.puts erb.result(binding)
+      end
+    }
+  end
+
+  File.open('/dev/null', 'w') do |file|
+    puts "Erubis " + '-' * 40
+    puts Benchmark::CAPTION
+    puts Benchmark.measure{
+      count.times do
+        erubis = Erubis::FastEruby.new(File.read(testname + '.erb'))
+        file.puts erubis.result(binding)
+      end
+    }
+  end
+
   Dir.chdir '..'
 end
 
 test_tmpl('test_var1', varset1);
 test_tmpl('test_var2', varset1);
+
+__END__
 test_tmpl('test_var3', varset1, varset2);
 test_tmpl('test_if1',  varset1);
 test_tmpl('test_if2',  varset1);
